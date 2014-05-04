7a57bddf3039d0704ab47209dce26d51f9ab49ee
fro_ozen <fro_ozen@gmx.de>
2014-05-04 15:52:47 +0200

    Implemented moveable cursor in notebook_selection_scene

--- notebook_selection_scene.py
+++ notebook_selection_scene.py
@@ -5,10 +5,13 @@ import notebook_editing_scene
 
 index = 0
 editing = False
+insert_index = -1
+insert_pos = []
 
 def handle_input(scr, c):
     global editing
     global index
+    global insert_index
 
     if not editing:
         if c == ord("q"):
@@ -26,11 +29,13 @@ def handle_input(scr, c):
             return True
 
         elif c in [ord("a"), curses.KEY_ENTER, 10]:
+            insert_index = len(__get_selected_notebook().name)
             editing = True
 
         elif c in [ord("o"), ord("n")]:
             notebooks.notebook_list.append(notebooks.Notebook("", notebooks.Node()))
             index = len(notebooks.notebook_list) - 1
+            insert_index = len(__get_selected_notebook().name)
             editing = True
 
         elif c in [ord("d"), 330]: #Delete key
@@ -41,13 +46,24 @@ def handle_input(scr, c):
     else:
         if c in range(32, 126): #normal characters
             __get_selected_notebook().name += chr(c)
+            insert_index += 1
 
         elif c == curses.KEY_BACKSPACE:
             __get_selected_notebook().name = __get_selected_notebook().name[0:len(__get_selected_notebook().name) - 1]
+            insert_index -= 1
 
         elif c in [27, curses.KEY_ENTER, 10]: #Escape and Enter
             __get_selected_notebook().mother.name = __get_selected_notebook().name
             editing = False
+            insert_index = -1
+
+        elif c == curses.KEY_LEFT:
+            if insert_index > 0:
+                insert_index -= 1
+
+        elif c == curses.KEY_RIGHT:
+            if insert_index < len(__get_selected_notebook().name):
+                insert_index += 1
 
     return True
 
@@ -71,19 +87,26 @@ def redraw(scr):
     scr.clear()
     __draw_notebooks(scr)
     __draw_mode(scr)
-    scr.move(scr.getmaxyx()[0] - 1, scr.getmaxyx()[1] - 1)
+
+    if (not insert_index == -1) and scr.getmaxyx()[0] > insert_pos[0] and scr.getmaxyx()[1] > insert_pos[1]:
+        scr.move(insert_pos[0], insert_pos[1])
+    
+    else:
+        scr.move(scr.getmaxyx()[0] - 1, scr.getmaxyx()[1] - 1)
 
 def __draw_notebooks(scr):
+    global insert_pos
+
     pos = [2, 2]
 
     for notebook in notebooks.notebook_list:
         if pos[0] < scr.getmaxyx()[0] and pos[1] < scr.getmaxyx()[1]:
-            scr.move(pos[0], 0)
-            scr.clrtoeol()
-
             notebook_str = ("[ %s ]" % notebook.name)[0:scr.getmaxyx()[1] - pos[1]]
             if notebook is notebooks.notebook_list[index]:
                 scr.addstr(pos[0], pos[1], notebook_str, curses.color_pair(1))
+                
+                if editing and (not insert_index == -1):
+                    insert_pos = [pos[0], pos[1] + 2 + insert_index]
 
             else:
                 scr.addstr(pos[0], pos[1], notebook_str)
